e1168266b01ac865ae5692177c62321737a55ab3
Elahe <91415385+Eli-Shirdel@users.noreply.github.com>
2022-03-28 20:47:18 +0430

    Update collapse.js

--- collapse.js
+++ collapse.js
@@ -1,7 +1,5 @@
 /* ========================================================================
- * Bootstrap: collapse.js v3.0.0
 
- * 
  * 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
