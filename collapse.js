b99d04bd9c4e8caaef43f1f062e61433b3888e57
Elahe <91415385+Eli-Shirdel@users.noreply.github.com>
2022-04-25 10:35:22 +0430

    Update collapse.js

--- collapse.js
+++ collapse.js
@@ -2,7 +2,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ 
  * Unless required by applicable law or agreed to in writing, software
  * distributed ) this.toggle
    && this.$parent.find('> .
